6accddcff0c486f1009366202ace6d26cdd67e8b
rajendra-aryan <rajendraaryansahu0173@gmail.com>
2025-08-13 21:18:21 +0530

    basics of JS(part2)

---
+++ JS/Part2/nonPrimitives.js
@@ -0,0 +1,8 @@
+let obj1 ={name:"a",id:1 }
+
+console.log(obj1);
+console.log(typeof obj1);
+console.log(obj1.id);
+
+let today = new Date()
+console.log(today.getDate());
---
+++ JS/Part2/primitives.js
@@ -0,0 +1,28 @@
+let balance = 120
+console.log(typeof balance);
+
+let anotherBalance = new Number(120)
+console.log(typeof anotherBalance);
+
+let isActive =true
+let isReallyActive = new Boolean(true)
+
+let first;
+console.log(first);
+
+let second = null;
+console.log(second);
+
+let third = undefined;
+console.log(third);
+
+let string = "hi"
+let string1 = "hi1"
+
+let total = string+string1
+
+console.log(`Hello ${total}`);
+
+let sm1 = Symbol("Sharky")
+let sm2 = Symbol()
+console.log(sm1==sm2);
