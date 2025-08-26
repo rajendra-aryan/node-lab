67ca5c050b2f19bc52ed1a56554621e715ea08cd
rajendra-aryan <rajendraaryansahu0173@gmail.com>
2025-08-26 14:49:51 +0530

    Book Store API via RESTful API with CRUD features

---
+++ express book store/index.js
@@ -0,0 +1,74 @@
+import express from "express";
+
+const app = express()
+const port = 5000;
+
+//In memory DB
+const books= [
+    { id: 1 , title: "Book One", author: "Author One" },
+    { id: 2 , title: "Book Two", author: "Author Two" },
+    { id: 3 , title: "Book Three", author: "Author Three" }
+];
+
+
+//Middlewares(Plugins)
+app.use(express.json());
+
+
+//Routes
+app.get('/books', (req, res)=>{
+    // res.setHeader('x-aryan', "Rajendra")
+    res.json(books)
+})
+
+
+app.get('/books/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        return res.status(400).json({error: `ID must be of type number`})
+    }
+
+    const book = books.find((e)=>e.id===id) // Select * from books where id = {id}
+
+    if(!book){
+        return res
+        .status(404)
+        .json({error: `Book with id: ${id} doesn't exists!`}) 
+    }
+
+    return res.json(book)
+})
+
+app.post('/books', (req,res)=>{
+    // console.log(req.headers)
+    // console.log(req.body)
+    const {title,author} =  req.body;
+    if(!title || title==='') return res.status(400).json({error: `title is required`})
+    if(!author || author==='') return res.status(400).json({error: `author is required`})
+    
+    const id = books.length + 1;
+    const book = {id, title, author}
+    books.push(book)
+
+    return res.status(201).json({message: `Book created success`, id})
+})
+
+app.delete('/books/:id', (req,res)=>{
+    const id = parseInt(req.params.id)
+
+    if(isNaN(id)){
+        return res.status(400).json({error: `ID must be of type number`})
+    }
+
+    const index = books.findIndex(e => e.id===id)
+    if(index<0) 
+        return res
+        .status(404)
+        .json({error: `Book with id:${id} doesn't exists`})
+
+    books.splice(index,1);
+    return res.status(200).json({message: `Book deleted`})
+})
+
+app.listen(port,() => console.log(`HTTP server is running on port: ${port}`)) 
