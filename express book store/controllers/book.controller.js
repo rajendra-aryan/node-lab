ae47ec506a5108cedfc14ab4beb58454696071b1
rajendra-aryan <rajendraaryansahu0173@gmail.com>
2025-08-27 11:35:03 +0530

    Refactoring express book store by MVC pattern

---
+++ express book store/controllers/book.controller.js
@@ -0,0 +1,60 @@
+import {books} from "../models/db.js";
+
+
+const getAllBooks = function(req, res){
+    // res.setHeader('x-aryan', "Rajendra")
+    res.json(books)
+}
+
+const getBookByID = function(req,res){
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        return res.status(400).json({error: `ID must be of type number`})
+    }
+
+    const book = books.find((e)=>e.id===id) // Select * from books where id = {id}
+
+    if(!book){
+        return res
+        .status(404)
+        .json({error: `Book with id: ${id} doesn't exists!`}) 
+    }
+
+    return res.json(book)
+}
+
+const createBook = function(req,res){
+    // console.log(req.headers)
+    // console.log(req.body)
+    const {title,author} =  req.body;
+    if(!title || title==='') return res.status(400).json({error: `title is required`})
+    if(!author || author==='') return res.status(400).json({error: `author is required`})
+    
+    const id = books.length + 1;
+    const book = {id, title, author}
+    books.push(book)
+
+    return res.status(201).json({message: `Book created success`, id})
+}
+
+const deleteBook = function(req,res){
+    const id = parseInt(req.params.id)
+
+    if(isNaN(id)){
+        return res.status(400).json({error: `ID must be of type number`})
+    }
+
+    const index = books.findIndex(e => e.id===id)
+    if(index<0) 
+        return res
+        .status(404)
+        .json({error: `Book with id:${id} doesn't exists`})
+
+    books.splice(index,1);
+    return res.status(200).json({message: `Book deleted`})
+}
+
+
+
+export {getAllBooks, getBookByID, createBook, deleteBook}
--- express book store/routes/book.routes.js
+++ express book store/routes/book.routes.js
@@ -1,63 +1,16 @@
 import express from "express";
-import {books} from "../db/config.js";
 import loggerMiddleware from "../middlewares/logger.js";
-
+import {getAllBooks, getBookByID, createBook, deleteBook} from "../controllers/book.controller.js"
 
 const router = express.Router()
 
-router.get('/', (req, res)=>{
-    // res.setHeader('x-aryan', "Rajendra")
-    res.json(books)
-})
+router.get('/', getAllBooks)
 
-router.get('/:id',loggerMiddleware, (req,res)=>{
-    const id = parseInt(req.params.id);
+router.get('/:id',loggerMiddleware, getBookByID)
 
-    if(isNaN(id)){
-        return res.status(400).json({error: `ID must be of type number`})
-    }
+router.post('/',createBook )
 
-    const book = books.find((e)=>e.id===id) // Select * from books where id = {id}
-
-    if(!book){
-        return res
-        .status(404)
-        .json({error: `Book with id: ${id} doesn't exists!`}) 
-    }
-
-    return res.json(book)
-})
-
-router.post('/', (req,res)=>{
-    // console.log(req.headers)
-    // console.log(req.body)
-    const {title,author} =  req.body;
-    if(!title || title==='') return res.status(400).json({error: `title is required`})
-    if(!author || author==='') return res.status(400).json({error: `author is required`})
-    
-    const id = books.length + 1;
-    const book = {id, title, author}
-    books.push(book)
-
-    return res.status(201).json({message: `Book created success`, id})
-})
-
-router.delete('/:id', (req,res)=>{
-    const id = parseInt(req.params.id)
-
-    if(isNaN(id)){
-        return res.status(400).json({error: `ID must be of type number`})
-    }
-
-    const index = books.findIndex(e => e.id===id)
-    if(index<0) 
-        return res
-        .status(404)
-        .json({error: `Book with id:${id} doesn't exists`})
-
-    books.splice(index,1);
-    return res.status(200).json({message: `Book deleted`})
-})
+router.delete('/:id', deleteBook)
 
 
 
